10b036e8306b8d82751d22e1ee6cb17534a7bd8e
rickys20 <73892020+rickys20@users.noreply.github.com>
2021-06-29 13:48:20 +0700

    form-order,form-product

---
+++ final_project/form-order.php
@@ -0,0 +1,21 @@
+<?php include ('config.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<title>Taqarra Shop</title>
+</head>
+
+<body>
+  <!--lalu saya menggunakan jumbotron untuk memberi desain untuk judul laman-->
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4" align="center">TAQARRA SHOP</h1>
+      <p class="lead" align="center">Selamat Datang di Taqarra Shop, Silahkan Order untuk Memesan Product dan Melihat Product pada Menu Product</p>
+    </div>
+  </div>
+</body>
