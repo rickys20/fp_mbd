e327685d40ecdf538a477be0e3233ba682a94b83
rickys20 <73892020+rickys20@users.noreply.github.com>
2021-06-29 12:38:24 +0700

    Update index.php

--- final_project/index.php
+++ final_project/index.php
@@ -7,7 +7,7 @@
   <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-	<title>PPDB Institut Teknologi Sepuluh Nopember</title>
+	<title>Taqarra Shop</title>
 </head>
 <!--dan lalu ini bagian body atau tubuh laman login saya-->
 <body>
